f818dde41ca8ceb7d8285f0b8f5d62ca907a0ae9
jiang_wh <jiang_wh@126.com>
2021-04-09 09:48:14 +0800

    注释测试代码

--- main.go
+++ main.go
@@ -1,48 +1,52 @@
 package main
 
 import (
+	"climber/getHots"
+	"climber/webPublish"
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	//"github.com/jinzhu/gorm"
+	//_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"time"
 )
 
-//func main() {
-//	fmt.Println("climer server start ...")
+func main() {
+	fmt.Println("climer server start ...")
+
+	timer := time.NewTimer(60 * time.Second)
+	go func() {
+		getHots.Collect()
+		for {
+			timer.Reset(60 * 5 * time.Second) // 这样来复用 timer 和修改执行时间
+			select {
+			case <-timer.C:
+				getHots.Collect()
+			}
+		}
+	}()
+	webPublish.Publish()
+
+}
+
 //
-//	timer := time.NewTimer(60 * time.Second)
-//	go func() {
-//		getHots.Collect()
-//		for {
-//			timer.Reset(60 * 5 * time.Second) // 这样来复用 timer 和修改执行时间
-//			select {
-//			case <-timer.C:
-//				getHots.Collect()
-//			}
-//		}
-//	}()
-//	webPublish.Publish()
-//
+//type Product struct {
+//	gorm.Model //嵌入常用字段
+//	Code  string
+//	Price uint
 //}
 
-type Product struct {
-	gorm.Model //嵌入常用字段
-	Code  string
-	Price uint
-}
-
-func main() {
-	db, err := gorm.Open("sqlite3", "lite.db")
-	if err != nil {
-		panic(err)
-	}
-	//关闭数据库连接
-	defer db.Close()
-
-	//创建表
-	db.AutoMigrate(&Product{})
-
-	product := &Product{Code: "No.002", Price: 520}
-	fmt.Println(db.NewRecord(product)) //output: true
-	db.Create(product)
-	fmt.Println(db.NewRecord(product)) //output: false
-}
+//func main() {
+//	db, err := gorm.Open("sqlite3", "lite.db")
+//	if err != nil {
+//		panic(err)
+//	}
+//	//关闭数据库连接
+//	defer db.Close()
+//
+//	//创建表
+//	db.AutoMigrate(&Product{})
+//
+//	product := &Product{Code: "No.002", Price: 520}
+//	fmt.Println(db.NewRecord(product)) //output: true
+//	db.Create(product)
+//	fmt.Println(db.NewRecord(product)) //output: false
+//}
